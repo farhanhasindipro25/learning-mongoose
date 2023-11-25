684b7fa55ea293329292a6bb034fa028ea816f3a
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2023-11-26 03:20:05 +0600

    Added interface and schema of a user

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,100 @@
 import cors from "cors";
 import express, { Application, NextFunction, Request, Response } from "express";
+import { Schema } from "mongoose";
 
 const app: Application = express();
 app.use(cors());
 
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
-  next();
+  /*
+    Step1: Interface
+    Step2: Schema
+    Step3: Model
+    Step4: Database Query
+    */
+
+  //creating an interface
+  interface IUser {
+    id: string;
+    role: "student";
+    password: string;
+    name: {
+      firstName: string;
+      middleName?: string;
+      lastName: string;
+    };
+    dateOfBirth?: string;
+    gender: "male" | "string";
+    email: string;
+    contactNo: string;
+    emergencyContactNo: string;
+    presentAddress: string;
+    permanenetAddress: string;
+  }
+  // creating Schema using Interface
+  const userSchema = new Schema<IUser>({
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      middleName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    dateOfBirth: {
+      type: String,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    contactNo: {
+      type: String,
+      required: true,
+    },
+    emergencyContactNo: {
+      type: String,
+      required: true,
+    },
+    presentAddress: {
+      type: String,
+      required: true,
+    },
+    permanenetAddress: {
+      type: String,
+      required: true,
+    },
+  });
+
+  //   res.send("Hello World!");
+  //   next();
 });
 
 export default app;
